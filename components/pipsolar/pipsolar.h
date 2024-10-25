feec9244e0a686e7e683c0a152f6c40af259795f
Dave Prue <dave@prue.com>
2024-10-25 17:16:15 +0800

    Update pipsolar.h

--- components/pipsolar/pipsolar.h
+++ components/pipsolar/pipsolar.h
@@ -189,7 +189,7 @@ class Pipsolar : public uart::UARTDevice, public PollingComponent {
  protected:
   static const size_t PIPSOLAR_READ_BUFFER_LENGTH = 160;  // maximum supported answer length
   static const size_t COMMAND_QUEUE_LENGTH = 10;
-  static const size_t COMMAND_TIMEOUT = 10000;
+  static const size_t COMMAND_TIMEOUT = 7500;
   uint32_t last_poll_ = 0;
   void add_polling_command_(const char *command, ENUMPollingCommand polling_command);
   void empty_uart_buffer_();
