27d874d2896e06061b56d18c053e334232998e1a
Dave Prue <dave@prue.com>
2024-10-25 18:48:43 +0800

    Update __init__.py

--- components/pipsolar/sensor/__init__.py
+++ components/pipsolar/sensor/__init__.py
@@ -72,7 +72,7 @@ CONF_PV_CHARGING_POWER = "pv_charging_power"
 TYPES = {
     CONF_GRID_RATING_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
     CONF_GRID_RATING_CURRENT: sensor.sensor_schema(
@@ -106,7 +106,7 @@ TYPES = {
     ),
     CONF_BATTERY_RATING_VOLTAGE: sensor.sensor_schema(
         unit_of_measurement=UNIT_VOLT,
-        accuracy_decimals=1,
+        accuracy_decimals=0,
         device_class=DEVICE_CLASS_VOLTAGE,
     ),
     CONF_BATTERY_RECHARGE_VOLTAGE: sensor.sensor_schema(
